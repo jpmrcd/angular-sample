a40a2955ca896054ff8e60f960d3013684b79cf5
JPMercado <mercado.jp@gmail.com>
2019-09-22 19:01:13 +0800

    Add circleci config; rename project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Management
+# Angular sample
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.0-next.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'management'`, () => {
+  it(`should have as title 'Angular sample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('management');
+    expect(app.title).toEqual('Angular sample');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('management app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular sample app is running!');
   });
 });
